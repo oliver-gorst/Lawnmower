494bfe79e39b9eef430d43c459a27ad874b5ad2c
Oliver Gorst <oliver.gorst@outlook.com>
2026-04-24 21:22:53 -0400

    Initial Commit

---
+++ Lawnmower/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lawnmower
+//
+//  Created by Oliver Gorst on 4/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lawnmower/LawnmowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawnmowerApp.swift
+//  Lawnmower
+//
+//  Created by Oliver Gorst on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct LawnmowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
